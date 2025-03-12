97be0bc727bacf9c4c9010f19ff88e544fd5e891
EsteNoEsMonti <estenoesmonti@gmail.com>
2025-03-12 18:43:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-sky-500 text-white">
+      <header className="text-4xl font-bold">Argentina</header>
+      <p className="mt-4 text-lg text-gray-100">
+        Tierra de paisajes, pasión y cultura.
+      </p>
+    </div>
+  )
+}
+
+export default App
