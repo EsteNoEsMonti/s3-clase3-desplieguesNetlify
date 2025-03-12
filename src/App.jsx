280792312a09193237786c2cd002cf1dabc427c7
lmontiverosfvc <lmontivero@sfvc.edu.ar>
2025-03-12 19:07:19 -0300

    add img

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 
+import sol from '../public/assets/sol.svg'
+
 function App() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-sky-500 text-white">
+      <img src={sol} className="w-[150px]" />
       <header className="text-4xl font-bold">Argentina</header>
       <p className="mt-4 text-lg text-gray-100">
         Tierra de paisajes, pasión y cultura.
